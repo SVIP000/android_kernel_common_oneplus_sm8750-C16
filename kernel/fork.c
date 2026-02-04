fcfe95430034a883a75a3bc8bdd2dd9bbaee4332
whitewhale0612 <2136512929@qq.com>
2026-02-04 23:41:24 +0800

    ANDROID: SPED: Add vendor hooks in Scheduler
    SPED (Scheduler-based Privilege Elevation Detection) a Pixel protection
    that blocks privilege elevation attacks. Before scheduling a task for
    execution, it checks if there is a transition to uid/euid 0 (root) and
    if so, it blocks the execution.
    
    We need to register 3 vendor hooks:
     * When a task is created in copy_process()
     * Before a task is selected for execution in __pick_next_task()
     * When a task is terminated in __put_task_struct()
    
    The rationale on why we are using these functions can be found in the "Understanding the Linux Scheduler" tab of go/sped-cookbook
    
    NOTE: There is already the trace_task_newtask() hook in copy_process() so we will reuse it for process creation. Therefore we will add only 2 new hooks.
    
    1p: go/hyp-sched-lpe-detection
    design (detailed): go/sped-cookbook
    
    Bug: 403623944
    Test: None
    
    Change-Id: Iae0f223488e8c9c5050f69f11d8930ad9b14871f
    Signed-off-by: Kyriakos Ispoglou <ispo@google.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1006,6 +1006,9 @@ void __put_task_struct(struct task_struct *tsk)
 	WARN_ON(!tsk->exit_state);
 	WARN_ON(refcount_read(&tsk->usage));
 	WARN_ON(tsk == current);
+	
+	trace_android_vh_put_task(tsk);
+	put_dmabuf_info(tsk);
 #ifdef CONFIG_HMBIRD_SCHED
 	hmbird_free(tsk);
 #endif
--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6317,6 +6317,8 @@ __pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 		if (unlikely(p == RETRY_TASK))
 			goto restart;
 
+		trace_android_vh_chk_task(&p, rq);
+
 		/* Assume the next prioritized class is idle_sched_class */
 		if (!p) {
 			put_prev_task(rq, prev);
@@ -6340,6 +6342,7 @@ restart:
 #else
 	for_each_class(class) {
 		p = class->pick_next_task(rq);
+		trace_android_vh_chk_task(&p, rq);
 		if (p)
 			return p;
 	}
